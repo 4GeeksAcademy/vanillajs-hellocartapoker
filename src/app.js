081cb8821488910b276471e4780a9f281a2f9adf
XefDavid <156122937+XefDavid@users.noreply.github.com>
2024-03-19 18:51:43 +0000

    primercomit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("boton").addEventListener("click", () => {
+    let palo = generadorPalosPoker();
+    document.getElementById("card").innerHTML =
+      "<p class='paloArriba'>" +
+      palo +
+      "</p>" +
+      "<p class='numero'>" +
+      generadorNumeros() +
+      "</p>" +
+      "<p class='paloAbajo'>" +
+      palo +
+      "</p>";
+    let disparo = document.getElementById("disparo");
+    disparo.play();
+  });
+};
+
+const generadorPalosPoker = () => {
+  let palos = ["♦️", "♣️", "♥️", "♠️"];
+  let indicePalos = [Math.floor(Math.random() * palos.length)];
+  return palos[indicePalos];
+};
+
+const generadorNumeros = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "k"
+  ];
+  let indiceNumeros = Math.floor(Math.random() * numeros.length);
+  console.log(indiceNumeros);
+  return numeros[indiceNumeros];
 };
